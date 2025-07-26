150c92a6bd50d7a8564d79ac6fa4414042c06417
manjyuuya <kawai.yuya@icloud.com>
2025-07-27 04:59:47 +0900

    mainにfirebase初期化設定追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // ⬅️ 必須！
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
