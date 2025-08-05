2e70319d4ebb70c03089d377a8796581e3d821b3
manjyuuya <kawai.yuya@icloud.com>
2025-08-05 14:43:21 +0900

    checkPokerNameExists.ts削除

--- functions/src/user/index.ts
+++ functions/src/user/index.ts
@@ -1,4 +1,3 @@
-export {checkPokerNameExists} from "./checkPokerNameExists";
 export {generateQRCode} from "./generateQRCode";
 export {verifyQRCode} from "./verifyQRCode";
 export {createUserAccount} from "./createUserAccount";
